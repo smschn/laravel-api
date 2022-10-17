138d3f038c5e3ab26999d09c32299accda4d9bd7
Luca Somaschini <somas99@gmail.com>
2022-10-17 17:10:11 +0200

    Added main component, installed axios globally

--- resources/js/front.js
+++ resources/js/front.js
@@ -1,6 +1,17 @@
-// questo file gestisce js per la parte front-office, cioè di tutte quelle routes visibili a chi NON ha fatto l'accesso.
+// questo file gestisce js per la parte pubblicamente accessibile del sito,
+// cioè accessibile senza login.
 
-window.Vue = require('vue'); // dichiaro Vue globalmente legandola all'oggetto window (la finestra del browser).
+// dichiaro Vue globalmente legandola all'oggetto window (la finestra del browser).
+window.Vue = require('vue'); 
+
+// lego axios all'oggetto window: così posso usarlo in tutti i componenti senza doverlo importare in ciascuno di essi,
+// perché viene caricato con la finestra (window) del browser.
+window.Vue = require('axios'); 
+
+/*
+// specifico che tutte le richieste axios fatte al server siano fatte in json
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+*/
 
 import Vue from 'vue'; // importo Vue.
 import App from './views/App'; // importo il componente globale <App> da <\resources\js\views\App.vue>.
--- resources/js/views/App.vue
+++ resources/js/views/App.vue
@@ -1,5 +1,5 @@
 <!--
-    componente globale:
+    questo file è un componente globale:
     funge da involucro per tutta la parte della web app accessibile a chi NON ha effettuato il login.
     qui importo, registro ed uso gli altri componenti creati per questa parte del sito pubblicamente accessibile.
 -->
@@ -7,17 +7,20 @@
 <template>
     <div>
         <MyHeader />
+        <MyMain />
     </div>
 </template>
 
 <script>
     // importo i componenti
     import MyHeader from '../components/MyHeader.vue';
+    import MyMain from '../components/MyMain.vue';
 
     export default {
         name: 'App',
         components: {
-            MyHeader
+            MyHeader,
+            MyMain
         }
     }
 </script>
